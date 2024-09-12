87488ef90f7a53dd18a041615863c140d432727d
daniel <daniel.juarez@ulv.edu.mx>
2024-09-12 07:37:44 -0600

    Creacion inicial de widgets custom staticos

---
+++ lib/presentation/screens/home/home_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:h2o_client_app/presentation/widgets/appBar/app_bar_widget.dart';
+import 'package:h2o_client_app/presentation/widgets/bottomNavigatorBar/bottom_navigator_bar_widget.dart';
+
+class HomeScreen extends StatelessWidget {
+
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+
+    return const Scaffold(
+        appBar: AppBarCustom(),
+        bottomNavigationBar: BottomNavigatorBarCustom(),
+        backgroundColor: Color(0xFFF0F5F5),
+        
+        );
+  }
+}
+
+
