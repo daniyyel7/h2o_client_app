bb08511f79c4502e7b238d1924ab5f3777a3511a
daniel <daniel.juarez@ulv.edu.mx>
2024-09-18 11:29:13 -0600

    modificacion de widgets y appbar

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:h2o_client_app/presentation/widgets/appBar/app_bar_widget.dart';
 import 'package:h2o_client_app/presentation/widgets/bottomNavigatorBar/bottom_navigator_bar_widget.dart';
+import 'package:h2o_client_app/presentation/widgets/searachBar/serach_bar_custom.dart';
+import 'package:h2o_client_app/presentation/widgets/textFormField/text_form_field_custom.dart';
 
 class HomeScreen extends StatelessWidget {
 
@@ -14,7 +16,6 @@ class HomeScreen extends StatelessWidget {
         appBar: AppBarCustom(),
         bottomNavigationBar: BottomNavigatorBarCustom(),
         backgroundColor: Color(0xFFF0F5F5),
-        
         );
   }
 }
