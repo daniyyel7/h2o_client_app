a64b25c871790599dbfc9ee614996619f1b51bec
Sheila Rodriguez <sheila.rodriguez@ulv.edu.mx>
2024-09-27 08:33:13 -0600

    Resolución de conflictos en Cards Product Custom

--- lib/presentation/widgets/cards/cards_product_custom.dart
+++ lib/presentation/widgets/cards/cards_product_custom.dart
@@ -1,73 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:h2o_client_app/presentation/widgets/cards/add_minus_widget.dart';
 
 class CardsProductCustom extends StatelessWidget {
   final String titleCard;
   final double height;
 
-  const CardsProductCustom({
-    super.key, 
-    required this.titleCard,
-    required this.height,
-    });
+  const CardsProductCustom(
+      {super.key, required this.titleCard, required this.height});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Card(
-      color: Colors.white,
-      child: SizedBox(
-        height: height,
-        width: 350,
-        child: 
-            Row(
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: Center(
+          child: Card(
+        color: Colors.white,
+        child: SizedBox(
+            height: height,
+            width: 350,
+            child: Row(
               children: [
-            const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: SizedBox(
-                height: 180,
-                width: 99,
-                child: Image(
-                  image: AssetImage('assets/img/logo.png')
+                const _ImageProdcutCard(
+                  urlImage: 'assets/img/logo.png',
                 ),
-              ),
-            ),
                 Column(
                   children: [
                     const Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        SizedBox(height: 10,),
-                        Text('Titulo', style: TextStyle( fontSize: 18, fontWeight: FontWeight.w600)),
+                        SizedBox(height: 10),
+                        _TitleProduct(title: 'Garrafón',),
                         SizedBox(
-                          height: 80,
-                          width: 230,
-                          child: Text('Descripcion del producto sdjdsh asdhasdhuad asdhauhd sajdbjabdashdhadjhsjhda', style: TextStyle( fontSize: 12))),
+                            height: 80,
+                            width: 220,
+                            child: _DescriptionProduct(description: 'Descripcion del producto asdasdasdasda sadas das dasdasdasdasd asdasdasdeffwefwef',)),
                       ],
                     ),
                     Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        const Text('Precio', style: TextStyle( fontSize: 12, fontWeight: FontWeight.w600)),
+                        const Text('Precio',
+                            style: TextStyle(
+                                fontSize: 12, fontWeight: FontWeight.w600)),
                         Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
+                          mainAxisAlignment: MainAxisAlignment.start,
                           children: [
-                            const SizedBox(width: 8),
-                            TextButton(
-                              child: const Text('Si'),
-                              onPressed: () {/* ... */},
-                            ),
-                            SizedBox(
-                              height: 40,
-                              width: 100,
-                              child: TextButton(
-                                style: TextButton.styleFrom(
-                                  backgroundColor: const Color(0xFFFFC40F),
-                                  
-                                ),
-                                child: const Text('Agregar'),
-                                onPressed: () {},
-                              ),
-                            ),
+                            const AddMinusCustom(),
+                            _ButtonAdd(),
                           ],
                         ),
                       ],
@@ -75,8 +54,80 @@ class CardsProductCustom extends StatelessWidget {
                   ],
                 ),
               ],
-            )
+            )),
+      )),
+    );
+  }
+}
+
+
+
+class _ButtonAdd extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 40,
+      width: 100,
+      child: TextButton(
+        style: const ButtonStyle(
+          backgroundColor: WidgetStatePropertyAll(Colors.amber),
+          
+        ),
+        child: const Text(
+          'Agregar',
+          style: TextStyle(color: Colors.black),
+        ),
+        onPressed: () {},
+        
       ),
-    ));
+    );
+  }
+}
+
+class _DescriptionProduct extends StatelessWidget {
+  final String description;
+  const _DescriptionProduct({
+    required this.description,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+        description,
+        style: const TextStyle(fontSize: 12));
+  }
+}
+
+class _TitleProduct extends StatelessWidget {
+  final String title;
+  const _TitleProduct({
+    required this.title,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(title,
+        style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w600));
+  }
+}
+
+class _ImageProdcutCard extends StatelessWidget {
+  final String urlImage;
+
+  const _ImageProdcutCard({
+    required this.urlImage,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: SizedBox(
+        height: 180,
+        width: 99,
+        child: Image(image: AssetImage(urlImage)),
+      ),
+    );
   }
 }
